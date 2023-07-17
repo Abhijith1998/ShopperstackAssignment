6ec5d569180232a93e313a9a4d446be9e101f575
abhis <abhis@Abhijith>
2023-07-17 17:01:32 +0530

    first push

---
+++ practiceJenkins/src/test/java/practiceJenkins/SmokeTestCase.java
@@ -0,0 +1,10 @@
+package practiceJenkins;
+
+import org.testng.annotations.Test;
+
+public class SmokeTestCase {
+	@Test
+	public void demo() {
+		System.out.println("belongs to smoke");
+	}
+}
---
+++ practiceJenkins/src/test/java/practiceJenkins/SmokeTestCase2.java
@@ -0,0 +1,10 @@
+package practiceJenkins;
+
+import org.testng.annotations.Test;
+
+public class SmokeTestCase2 {
+	@Test
+	public void demo() {
+		System.out.println("2nd smoke test case is running");
+	}
+}
